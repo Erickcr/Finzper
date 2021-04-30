f51c538f66744d65a824862011084318bb8a01b8
Erickcr <Erick-96-17@hotmail.com>
2021-04-29 21:51:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/finzper/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.finzper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.inicioFragment -> {
+                val inicioFragment = InicioFragment.newInstance()
+                openFragment(inicioFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.analisisFragment -> {
+                val analisisFragment = AnalisisFragment.newInstance()
+                openFragment(analisisFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.confFragment -> {
+                val confFragment = ConfFragment.newInstance()
+                openFragment(confFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
